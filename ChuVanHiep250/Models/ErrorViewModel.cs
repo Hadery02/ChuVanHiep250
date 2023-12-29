19f29269494ccd288b553e69152805fef2c9ebf8
Hadery02 <chuvanhiep1211@gmail.com>
2023-12-29 13:36:35 +0700

    first

---
+++ ChuVanHiep250/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ChuVanHiep250.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
